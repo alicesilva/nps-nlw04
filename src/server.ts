706e671d5c63ec305c030197c03f3367ac9f4ecd
alice.fernandes <alice.fernandes@splab.ufcg.edu.br>
2021-02-22 13:24:35 -0300

    aula 01 - rumo ao próximo nível

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+
+const app = express();
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica
+ */
+
+
+app.get("/", (request, response) => {
+    return response.json({message: "Hello World - NLW04"})
+});
+
+// 1º param => Rota(Recurso API)
+// 2º param => request, response
+
+app.post("/", (request, response) => {
+    //Recebeu os dados para salvar
+    return response.json({message: "Os dados foram salvos com sucesso!"})
+});
+
+
+app.listen(3333, () => console.log("Server is running"));
